9431d7967f1ee078d91304c515fdd7d4b9273968
rahuldhodapkar <rahul.m.dhodapkar@gmail.com>
2018-04-01 13:46:35 -0400

    clean up models file

--- application/models.py
+++ application/models.py
@@ -1,21 +1,16 @@
 from index import client, bcrypt
 from datetime import datetime
 
-from sqlalchemy.sql import func
-import sqlalchemy.dialects.postgresql as pgtypes
-
-
+""" Rough Schema
+{
+    email: String
+    password: String # salted, hashed password
+}
+"""
 class User():
 
     db_name = "peeredit"
     coll_name = "user"
-
-    """ Rough Schema
-    {
-        email: String
-        password: String # salted, hashed password
-    }
-    """
 
     @classmethod
     def create_new_user(cls, userobj):
@@ -43,46 +38,84 @@ class User():
         else:
             return None
 
-class Comment():
-
-    db_name = "peeredit"
-    coll_name = "comment"
-
-    """ Rough Schema
-    {
-        _id: String, # slug extension
-        articleId: ObjectId(),
-        content: {
-            text: String
+""" Rough Schema
+{
+    _id: String, # slug extension
+    articleId: ObjectId(),
+    content: {
+        text: String
+    },
+    position {
+        boundingRect: {
+            x1: Number,
+            y1: Number,
+            x2: Number,
+            y2: Number,
+            width: Number,
+            height: Number
         },
-        position {
-            boundingRect: {
+        rects: [ 
+            {
                 x1: Number,
                 y1: Number,
                 x2: Number,
                 y2: Number,
                 width: Number,
                 height: Number
-            },
-            rects: [ 
-                {
-                    x1: Number,
-                    y1: Number,
-                    x2: Number,
-                    y2: Number,
-                    width: Number,
-                    height: Number
-                } 
-            ],
-            pageNumber: Number
-        },
-        comment: {
-            text: String
-        }
+            } 
+        ],
+        pageNumber: Number
+    },
+    comment: {
+        text: String
     }
-    """
+}
+"""
+class Comment():
+
+    db_name = "peeredit"
+    coll_name = "comment"
 
     @classmethod
     def create_new_comment(cls, comment):
         return client[cls.db_name][cls.coll_name].insert_one(comment)
+
+
+""" Rough Schema
+{
+    _id: ObjectId(),
+    hash: String,
+    links: [
+        {
+            url: String,
+            lastValidated: Date(),
+            reporter: {
+                _id: ObjectId()
+                , email: String
+            } 
+        }
+    ],
+}
+"""
+class Article():
+
+    db_name = "peeredit"
+    coll_name = "article"
+
+    @classmethod
+    def index_new_article(cls, article):
+        """ Index a new article, hash, and store """
+        pass
+
+    @classmethod
+    def get_article_with_id(cls, idx):
+        """ Get article by id """
+        return client[cls.db_name][cls.coll_name].find_one({"_id": idx})
+
+
+
+
+
+
+
 
